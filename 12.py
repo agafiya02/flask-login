e602a85f2ea691b4a9f77e4a9e773bc0d10c2a98
agafiya02 <151033863+agafiya02@users.noreply.github.com>
2024-03-04 17:19:48 +0500

    Обработчик формы авторизации

---
+++ 12.py
@@ -0,0 +1,74 @@
+from flask import Flask, render_template, redirect
+from data import session
+from data import users
+from data.jobs import Jobs
+import datetime
+from flask_login import LoginManager, login_user
+from forms.user import RegisterForm, LoginForm
+
+app = Flask(__name__)
+app.config['PERMANENT_SESSION_LIFETIME'] = datetime.timedelta(
+    days=365
+)
+app.config['SECRET_KEY'] = 'grgrgr'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+session.global_init("db/users.db")
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = session.create_session()
+    return db_sess.query(users.User).get(user_id)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_sess = session.create_session()
+        user = db_sess.query(users.User).filter(users.User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login.html', title='Авторизация', form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = session.create_session()
+        if db_sess.query(users.User).filter(users.User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = users.User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+
+
+@app.route('/')
+def main():
+    db_sess = session.create_session()
+    jobs = db_sess.query(Jobs)
+    return render_template('jobs.html', title='Работа', jobs=jobs)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1')
