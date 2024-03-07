a2f97e4b17147bc1482580c6e0b5044a3da181b6
agafiya02 <151033863+agafiya02@users.noreply.github.com>
2024-03-07 18:20:57 +0500

    Добавление работы

--- API/jobs_api.py
+++ API/jobs_api.py
@@ -4,7 +4,7 @@ import flask
 from data.jobs import Jobs
 
 jobs_blueprint = flask.Blueprint(
-    'news_api',
+    'jobs_api',
     __name__,
     template_folder='templates'
 )
@@ -16,20 +16,44 @@ def get_jobs():
     jobs = db_sess.query(Jobs).all()
     return jsonify(
         {
-            'news':
+            'jobs':
                 [item.to_dict() for item in jobs]
         }
     )
 
 
 @jobs_blueprint.route('/api/jobs/<int:jobs_id>', methods=['GET'])
-def get_one_news(jobs_id):
+def get_one_jobs(jobs_id):
     db_sess = session.create_session()
     jobs = db_sess.query(Jobs).get(jobs_id)
     if not jobs:
         return make_response(jsonify({'error': 'Not found'}), 404)
     return jsonify(
         {
-            'news': jobs.to_dict()
+            'jobs': jobs.to_dict()
         }
     )
+
+
+@jobs_blueprint.route('/api/jobs', methods=['POST'])
+def create_jobs():
+    if not request.json:
+        return make_response(jsonify({'error': 'Empty request'}), 400)
+    elif not all(key in request.json for key in
+                 ['title', 'content', 'user_id', 'is_private']):
+        return make_response(jsonify({'error': 'Bad request'}), 400)
+    db_sess = session.create_session()
+
+    jobs = Jobs(
+        team_leader=request.json['team_leader'],
+        job=request.json['bob'],
+        content=request.json['content'],
+        work_size=request.json['work_size'],
+        colloboration=request.json['colloboration'],
+        start_date=request.json['start_date'],
+        end_date=request.json['end_date'],
+        is_finished=request.json['is_finished']
+    )
+    db_sess.add(jobs)
+    db_sess.commit()
+    return jsonify({'id': jobs.id})
